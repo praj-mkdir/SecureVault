2cfd4bbb1ad20d6bca615cb9cae9d2989d28b77b
Prajwal Patil <prajwalpatil3764@gmail.com>
2025-06-03 15:44:55 +0530

    v0.init-setup

---
+++ src/test/java/com/praj/secureVault/SecureVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praj.secureVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
