f1c85f68705ed94ca2053f41156be2269c011e9f
Prajwal Patil <prajwalpatil3764@gmail.com>
2025-06-25 22:03:22 +0530

    feat: add the storageType enums

---
+++ src/main/java/com/praj/secureVault/util/enums/StorageType.java
@@ -0,0 +1,28 @@
+package com.praj.secureVault.util.enums;
+
+
+import java.util.Arrays;
+
+//Add the storageType as per your requirement here
+public enum StorageType {
+    LOCAL("local"),
+    S3("s3"),
+    AZURE("azure");
+
+    private final String type;
+
+    StorageType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public static StorageType fromString(String value) {
+        return Arrays.stream(StorageType.values())
+                .filter(t -> t.getType().equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid storage type: " + value));
+    }
+}
