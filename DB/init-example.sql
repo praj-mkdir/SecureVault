b6cfda33fcac122dfaf6c21a195f5fd14f8e8116
Prajwal Patil <prajwalpatil3764@gmail.com>
2025-07-01 21:52:36 +0530

    feat: Init db script

---
+++ DB/init-example.sql
@@ -0,0 +1,9 @@
+-- Run this script to create the DB and user.
+-- Create a new database user
+CREATE USER <User-name-here> WITH PASSWORD '<User-password-here>'; -- change the password while going live
+
+-- Create the securevault database
+CREATE DATABASE <Database-name-here> OWNER <User-name-here>;
+
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON DATABASE <Database-name-here> TO <User-name-here>;
